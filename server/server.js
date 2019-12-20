ed7fa00b15167eb99441e7f2a7967768a5ea424a
insomniac807 <philipbutler25@gmail.com>
2019-12-20 09:15:55 +0000

    basic redis added. needs a lot of code clean up

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ const bodyParser =  require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
 
-let client = redis.createClient();
+var client = redis.createClient();
 
-client.on('connect', function() {
-    console.log("Connected to Redis");
-});
 
-const port = 3000;
+// client.on('connect', function() {
+//     console.log("Connected to Redis");
+// });
+
+// client.on('error',function() {
+//  console.log("Error in Redis");
+// })
+
+const port = 4000;
 const app = express();
 
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
@@ -26,14 +31,47 @@ app.use(methodOverride('_method'));
 
 app.get('/', function(req, res, next) {
     res.render('searchusers');
-    // particlesJS.load('particles-js', './views/layouts/particles.json', function(){
-    //     console.log("particles.json loaded");
-    // })
 });
 
-app.post('/api/bot/handleKarma', function(req, res, next){
-    let id = this.body.id;
-});
+
+//BACK END API STUFF ////
+
+// Cache middleware
+function cache(req, res, next) {
+    const { name } = req.params;
+    console.log("Name sent to server.... "+name);
+
+    client.get(name, (err, data) => {
+        if(err) throw err;
+
+        if(data !== null && data !== undefined && data !== "") {
+            console.log("data "+parseInt(data));
+            let newKarma = parseInt(data) + 1;
+            client.set(name, newKarma);
+            res.json({"karma": newKarma.toString()});
+        } else {
+            next();
+        }
+    })
+}
+//next middleware
+async function initUserKarma(req, res) {
+    try {
+        console.log("Fetching karma..");
+        const { name } = req.params;
+
+        client.set(name, 1);
+
+        res.send({name: 1});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    }
+}
+
+
+app.get('/api/bot/getUserKarma:name', cache, initUserKarma);
 
 app.listen(port, function(){
     console.log(`Server stared on port `+port+"!")
--- src/bot/modules/karma.js
+++ src/bot/modules/karma.js
@@ -1,3 +1,7 @@
+const fetch = require('node-fetch');
+const server = "http://localhost:4000";
+const apiGetKarma = "/api/bot/getUserKarma";
+
 class Karma {
     constructor(context) {
         this.context = context;
@@ -9,9 +13,13 @@ class Karma {
     }
 
     giveKarma() {
-        let reciever = this.message.substring(1, this.message.indexOf('++'));
-        let reply =  this.user+" sends karma to "+reciever;
-        this.context.reply(reply);
+        let kReciever = this.message.substring(1, this.message.indexOf('++'));
+        let query = server+apiGetKarma+kReciever;
+        fetch(query)
+            .then(res => res.json())
+            .then(body => {
+                this.context.reply(kReciever+" now has "+body.karma+" karma");
+            });
     }
 
     takeKarma() {
