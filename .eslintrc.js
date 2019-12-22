0e4faf8531b695378df777c90c48cc73f11e957b
SevenCatsHere <snowcolton@hotmail.com>
2019-12-21 20:55:40 -0500

    add eslint and editor config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+	root: true,
+	extends: 'airbnb-base',
+	parserOptions: { sourceType: 'script' },
+	env: { node: true },
+	rules: {
+		strict: [2, 'global'],
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+	},
+};
