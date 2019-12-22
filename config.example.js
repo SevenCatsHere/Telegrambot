c4f365f76c19ef441dc9bf4c503d104e424f8a45
Philip Butler <17027013+insomniac807@users.noreply.github.com>
2019-12-22 00:43:52 +0000

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 const config = {
-    BotApiKey: "INSERT TELEGRAM BOT API KEY HERE"
+    BotApiKey: "INSERT TELEGRAM BOT API KEY HERE",
+    Server: "SERVER HERE E.G www.website.com"
 }
 
 module.exports = config;
